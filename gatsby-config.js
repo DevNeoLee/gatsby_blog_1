6cb1dfb3143ea370073a2b1ce41fba0a0231ea88
DevNeoLee <devneolee@gmail.com>
2020-12-31 21:07:28 -0800

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Natural Vitamins`,
+    description: `What you eat is what you are! Eat healthy! Live happy!`,
+    author: `@Justin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
 
 import React from "react"
 import PropTypes from "prop-types"
@@ -37,9 +31,9 @@ const Layout = ({ children }) => {
         <footer style={{
           marginTop: `2rem`
         }}>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="#">Justin</a>
         </footer>
       </div>
     </>
